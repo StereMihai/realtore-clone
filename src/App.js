746ff46c6db5c41f28a5fad6d22a55f2d784e92a
Stere Mihai <mihai_stere@DESKTOP-29CGRE6.localdomain>
2022-12-09 22:32:39 +0200

    create routing and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
